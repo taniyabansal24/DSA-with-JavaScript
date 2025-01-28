7d91edfb0ad5563a4872bcdb788b636a14d3f8c2
Taniya Bansal <taniya1agarwal@gmail.com>
2025-01-28 11:08:04 +0530

    add q - sub arrays

---
+++ Array/subarraysOfArray.js
@@ -0,0 +1,17 @@
+// Subarrays of an Array 
+
+// Solution
+function Subarrays(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            let subarray = [];
+            for (let k = i; k <= j; k++) { // Loop from i to j to form the subarray
+                subarray.push(arr[k]);
+            }
+            console.log(subarray); // Print the subarray
+        }
+    }
+}
+
+// Example usage
+console.log(Subarrays([1, 2, 3, 4]));
